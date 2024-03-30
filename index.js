2fa7544395e1a92a7ac64b0c22c39b8c9b26d412
Jeferson Moreira <jefersonhmm82@gmail.com>
2024-03-30 18:47:55 -0300

    Desafio01

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Olá Mundo")
+// Criando Héroi
+const heroi = ("Grande Guerreiro")
+let xp = 1
+
+// Nível do Héroi
+const classe = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascedente", "Imortal", "Radiante"]
+switch (true) {
+    case xp < 1001:
+        console.log("O Herói de nome " + heroi + " está no nível de " + classe[0])
+        break
+    case xp < 2001:
+        console.log("O Herói de nome " + heroi + " está no nível de " + classe[1])
+        break
+    case xp < 5001:
+        console.log("O Herói de nome " + heroi + " está no nível de " + classe[2])
+        break
+    case xp < 7001:
+        console.log("O Herói de nome " + heroi + " está no nível de " + classe[3])
+        break
+    case xp < 8001:
+        console.log("O Herói de nome " + heroi + " está no nível de " + classe[4])
+        break
+    case xp < 9001:
+        console.log("O Herói de nome " + heroi + " está no nível de " + classe[5])
+        break
+    case xp < 10001:
+        console.log("O Herói de nome " + heroi + " está no nível de " + classe[6])
+        break
+}
